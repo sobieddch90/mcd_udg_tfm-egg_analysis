5bd3d3eb5c05982fe0e1ea21ea9b97b5dc5c7ddc
Elmo Chavez <sobieddch90@gmail.com>
2023-08-27 00:21:49 +0200

    third feature extraction

--- utils.py
+++ utils.py
@@ -8,7 +8,8 @@ All the functions required to extract features and classify AD and FTD from the 
 
 import pandas as pd
 import numpy as np
-from mne.time_frequency import tfr_multitaper, tfr_morlet
+import mne
+from mne.time_frequency import tfr_multitaper, tfr_morlet, psd_array_welch
 from scipy.stats import skew, kurtosis
 
 import matplotlib.pyplot as plt
@@ -28,9 +29,8 @@ from sklearn.svm import SVC
 import lightgbm as lgb
 
 #--------------------------#--------------------------#
-# First 
+# Feature Extraction from Stats, TFR and PSD
 #--------------------------#--------------------------#
-
 
 # Common variables
 metrics = ['mean','standard_deviation','variance','peak_to_peak','skewness','kurtosis']
@@ -125,3 +125,166 @@ def PSD_Features(epochs, fmin=0.5, fmax=45.0, method='multitaper'):
     psd_features = psd_features.astype(list(zip(column_names, dtypes)))
     
     return psd_features
+
+#--------------------------#--------------------------#
+# Feature Extraction from Frequency Bands and PSD
+#--------------------------#--------------------------#
+
+# Common Variables
+frequency_bands = {
+    'delta': (0.5, 4),
+    'theta': (4, 8),
+    'alpha': (8, 13),
+    'beta': (13, 30),
+    'gamma': (30, 100)
+}
+
+
+def get_Features_PSD(raw, channels=None, start_time = 0, end_time = 600, duration = 60.0, overlapping = 30.0):
+    # variables
+    sfreq = raw.info['sfreq']
+    
+    # Select channels
+    if channels==None: ch = raw.ch_names
+    else: ch = channels        
+    
+    # Get Epochs Data
+    data = mne.make_fixed_length_epochs(raw.copy().pick(ch).crop(tmin=start_time, tmax=end_time),
+                                    duration=duration, overlap=overlapping, preload=True, verbose='CRITICAL').get_data()
+    # Compute features
+    results = {}
+    for band, (fmin,fmax) in frequency_bands.items():
+        # Apply bandpass filter to select only the frequency band range
+        data_filtered = mne.filter.filter_data(data, sfreq=sfreq, l_freq=fmin, h_freq=fmax, verbose='CRITICAL')
+        # Compute the PSD using Welch's method
+        psd_all, freqs_all = psd_array_welch(data, sfreq=sfreq, verbose='CRITICAL')
+        psd, freqs = psd_array_welch(data_filtered, sfreq=sfreq, verbose='CRITICAL')
+        
+        # Compute metrics
+        total_power = psd.sum()
+        relative_power = total_power / psd_all.sum()
+        average_power = psd.mean()
+        #entropy = compute_spect_entropy(data_filtered, sfreq=sfreq)
+        entropy = -np.sum(np.log(psd) * psd)
+        std_dev = np.std(data_filtered)
+        peak_to_peak = np.ptp(data_filtered)
+        kurt = kurtosis(data_filtered)
+        skewness = skew(data_filtered)
+        # Store results
+        results[f'{band}_total_power'] = total_power
+        results[f'{band}_relative_power'] = relative_power
+        results[f'{band}_average_power'] = average_power
+        results[f'{band}_spectral_entropy'] = entropy
+        results[f'{band}_peak_to_peak'] = peak_to_peak
+        results[f'{band}_std_dev'] = std_dev
+        results[f'{band}_kurtosis'] = np.mean(kurt)
+        results[f'{band}_skewness'] = np.mean(skewness)
+        
+    return results
+    
+def get_Features_TFR(raw, channels=None, start_time = 0, end_time = 600, duration = 60.0, overlapping = 30.0, n_cycles=7, time_bandwidth=4.0):
+    # variables
+    sfreq = raw.info['sfreq']
+    
+    # Select channels
+    if channels==None: ch = raw.ch_names
+    else: ch = channels        
+    
+    # Get Epochs Data
+    epochs = mne.make_fixed_length_epochs(raw.copy().pick(ch).crop(tmin=start_time, tmax=end_time),
+                                    duration=duration, overlap=overlapping, preload=True, verbose='CRITICAL')
+    
+    # Compute features
+    results = {}
+    for band, (fmin,fmax) in frequency_bands.items():
+        # Get the TFR object
+        tfr = tfr_multitaper(epochs.copy().pick(ch), freqs=np.arange(fmin,fmax,1), n_cycles=n_cycles, time_bandwidth=time_bandwidth, return_itc=False, verbose='critical')
+        # Compute features
+        total_power = tfr.data.sum()
+        average_power = tfr.data.mean()
+        peak_power = tfr.data.max()
+        std_dev = tfr.data.std()
+        skewness = skew(tfr.data, axis=(1,2))
+        kurt = kurtosis(tfr.data, axis=(1,2))
+        # Store results
+        results[f'{band}_total_power'] = total_power
+        results[f'{band}_average_power'] = average_power
+        results[f'{band}_peak_power'] = peak_power
+        results[f'{band}_std'] = std_dev
+        results[f'{band}_kurtosis'] = np.mean(kurt)
+        results[f'{band}_skewness'] = np.mean(skewness)
+    
+    return results
+
+#--------------------------#--------------------------#
+# Predictions
+#--------------------------#--------------------------#
+def predictor(X, y, approach, frequencies, channels, features_names, selector_k, selector_perc):
+    # Define the classifiers
+    svm_classifier = SVC()
+    random_forest_classifier = RandomForestClassifier()
+    adaboost_classifier = AdaBoostClassifier()
+    xgboost_classifier = xgb.XGBClassifier()
+    lightgbm_classifier = lgb.LGBMClassifier()
+
+    classifiers = [svm_classifier, random_forest_classifier, adaboost_classifier, 
+                   xgboost_classifier, lightgbm_classifier]
+    classifier_names = ['SVM', 'Random Forest', 'AdaBoost', 
+                        'XGBoost', 'LightGBM']
+
+    # Create a cross-validator with 5 folds
+    n_splits = 5
+    test_size = 0.2
+
+    cv_method = [KFold(n_splits=n_splits, shuffle=True, random_state=42),
+                StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42),
+                StratifiedShuffleSplit(n_splits=n_splits, test_size=test_size, random_state=42)]
+    cv_method_names = ['KFold','StratifiedKFold','StratifiedShuffleSplit']
+
+    # Create the Feature Selector method
+    feat_sel_method =   [SelectKBest(score_func=f_classif, k=selector_k), SelectKBest(score_func=mutual_info_classif, k=selector_k),
+                        SelectPercentile(score_func=f_classif, percentile=selector_perc), SelectPercentile(score_func=mutual_info_classif, percentile=selector_perc)]
+    feat_sel_method_names = ['SelectKBest - f_classif','SelectKBest - mutual_info_classif',
+                            'SelectPercentile - f_classif', 'SelectPercentile - mutual_info_classif']
+    
+    # Run cross-validation for each classifier and each feature selection
+    results = []
+    for clf, clf_name in zip(classifiers, classifier_names):
+        print('Running',clf_name)
+        for cv, cv_name in zip(cv_method, cv_method_names):
+            for selector, sel_name in zip(feat_sel_method, feat_sel_method_names):
+                scores = []
+                for train_index, test_index in cv.split(X, y):
+                    # Split the data into training and testing sets based on the current fold indices
+                    X_train, X_test = X[train_index], X[test_index]
+                    y_train, y_test = y[train_index], y[test_index]
+                    
+                    # Feature Selection
+                    X_train_selected = selector.fit_transform(X_train, y_train)
+                    selected_index = selector.get_support()
+                    X_test_selected = selector.transform(X_test)
+                    
+                    # Train the classifier on the training data
+                    clf.fit(X_train_selected, y_train)
+                    
+                    # Make predictions on the test data
+                    y_pred = clf.predict(X_test_selected)
+                    
+                    # Calculate accuracy for this fold and append to the scores list
+                    accuracy = np.mean(y_pred == y_test)
+                    scores.append(accuracy)
+        
+                # Store results
+                result = {'Feature Extraction': approach, 
+                          'Frequencies':frequencies, 
+                          'Channels': channels,
+                          'Metrics': features_names,
+                          'Cross-Validate Method': cv_name,
+                          'Feature Selection': sel_name,
+                          'Metrics Selected':[features_names[i] for i, mask in enumerate(selected_index) if mask],
+                          'ML Model': clf_name,
+                          'Accuracy': np.mean(scores)}
+                
+                results.append(result)
+
+    return results
